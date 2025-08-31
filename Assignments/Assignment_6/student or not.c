5a1fff3fda0938221b3d4a172c5b786871a5d940
harshita-verma01 <harshugurjar98@gmail.com>
2025-08-31 22:10:57 +0530

    student or not

---
+++ Assignments/Assignment_6/student or not.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    float price, discount = 0.0;
+    char student;
+
+   
+    printf("Enter the price: ");
+    scanf("%f", &price);
+
+    printf("Are you a student? (y/n): ");
+    scanf(" %c", &student); 
+
+    isStudent(price,student);
+    
+//    if (student == 'y' || student == 'Y') {
+//        if (price > 500) {
+//            discount = price * 0.20;
+//        } else {
+//            discount = price * 0.10;
+//        }
+//    } else {
+//        if (price > 600) {
+//            discount = price * 0.15;
+//        } else {
+//            discount = 0.0;
+//        }
+//    }
+
+
+//    printf("Discount: %.2f\n", discount);
+//    printf("Final Price after discount: %.2f\n", price - discount);
+//
+//    return 0;
+}
+isStudent(int p,char c){
+	
+}
